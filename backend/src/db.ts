d43d86b49c6723c90da40af80e91a11b3513e5f1
eliseven-7 <feng.yinu@northeastern.edu>
2024-09-24 11:45:20 -0700

    Create a reusable connection pool

---
+++ backend/src/db.ts
@@ -0,0 +1,14 @@
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Create a connection pool
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+});
+
+export default pool;
